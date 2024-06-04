0ee750957d26ee7b0e4b6da98b0300d264af4b07
te210896 <te210896@gmail.com>
2024-06-04 12:37:11 +0900

    updated at 2024_06_04_12:37

--- src/test/java/jp/te4a/spring/boot/mybootapp10/MyBootAppTest.java
+++ src/test/java/jp/te4a/spring/boot/mybootapp10/MyBootAppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybootapp7ApplicationTests {
+class MyBootAppTest {
 
 	@Test
 	void contextLoads() {
