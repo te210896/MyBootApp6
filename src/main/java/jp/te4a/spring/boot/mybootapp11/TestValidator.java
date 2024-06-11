46d03eeece0b917c92dcddfb5666ce312872f580
te210896 <te210896@gmail.com>
2024-06-11 09:59:22 +0900

    created for assinment11.

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp11/TestValidator.java
@@ -0,0 +1,18 @@
+package jp.te4a.spring.boot.mybootapp11;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TestValidator implements ConstraintValidator<TestValid, String> {
+    String param;
+    @Override
+    public void initialize(TestValid tv){param = tv.param();}
+    @Override
+    public boolean isValid(String in, ConstraintValidatorContext ctxt) {
+        // inがnullの場合とparamと一致した場合にエラー
+        if (in == null) {
+            return false;
+        }
+        return !in.equals(param);
+    }
+}
